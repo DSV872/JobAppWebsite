f81a68b003a386c00a764a32e709815a720d3e98
DSVardhan <dasarisanthivardhan48@gmail.com>
2025-05-11 10:16:39 +0530

    log file aspect is added

--- src/main/java/com/dsv/SpringBootRest/LoggingAspect.java
+++ src/main/java/com/dsv/SpringBootRest/LoggingAspect.java
@@ -1,8 +1,7 @@
 package com.dsv.SpringBootRest;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -15,9 +14,22 @@ public class LoggingAspect {
     //syntax is return type class-name.method-name(args)
     @Before("execution(* com.dsv.SpringBootRest.service.JobService.getJob(..)) || execution(* com.dsv.SpringBootRest.service.JobService.updateJob(..))")
     public void logMethodCall(JoinPoint jp){ //hold on the log before information
-        LOGGER.info("Log method called"+jp.getSignature().getName());
+        LOGGER.info("Log method called "+jp.getSignature().getName());
     }
 
+    @After("execution(* com.dsv.SpringBootRest.service.JobService.getJob(..)) || execution(* com.dsv.SpringBootRest.service.JobService.updateJob(..))")
+    public void logMethodEx(JoinPoint jp){ //hold on the log before information
+        LOGGER.info("Log method executed "+jp.getSignature().getName());
+    }
 
+    @AfterThrowing("execution(* com.dsv.SpringBootRest.service.JobService.getJob(..)) || execution(* com.dsv.SpringBootRest.service.JobService.updateJob(..))")
+    public void logMethodCrash(JoinPoint jp){
+        LOGGER.info("Log method has some issues "+jp.getSignature().getName());
+    }
+
+    @AfterReturning("execution(* com.dsv.SrpingBootRest.service.JobService.getJob(..)) || execution(* com.dsv.SpringBootRest.service.JobService.updateJob(..))")
+    public void logMethodSuccess(JoinPoint jp){
+        LOGGER.info("Log method executed successfully "+jp.getSignature().getName());
+    }
 
 }
